48cd28bacc3d98ed55e2dec86b976442255ddcf6
cgray09 <47266486+cgray09@users.noreply.github.com>
2021-06-20 17:21:48 -0700

    Add files via upload

---
+++ my_shop/MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.WebUI.Controllers;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System.Web.Mvc;
+using MyShop.Core.ViewModels;
+using System.Linq;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCatgeoryContext = new Mocks.MockContext<ProductCategory>();
+
+            productContext.Insert(new Product());
+
+            HomeController controller = new HomeController(productContext, productCatgeoryContext);
+
+            // "ViewResult" represents the ActionResult that the method in the
+            // controller returns
+            var result = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            // tests if theres a total product count of 1
+            Assert.AreEqual(1, viewModel.Products.Count());
+
+        }
+    }
+}
